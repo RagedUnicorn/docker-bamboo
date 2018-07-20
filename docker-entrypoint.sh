3ab1ef5c05b7e5cddb4d57078ed2dc23c456806b
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-07-20 18:30:17 +0200

    Update entrypoint and dockery shell scripts with safer options * -e exit immediately upon error * -u treat unset variables as error * -o pipefail better error handling for pipes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,7 @@
 # @author Michael Wiesendanger <michael.wiesendanger@gmail.com>
 # @description launch script for confluence
 
-# abort when trying to use unset variable
-set -o nounset
+set -euo pipefail
 
 # start bamboo in foreground
 exec su-exec ${BAMBOO_USER} /opt/atlassian/bamboo/bin/start-bamboo.sh -fg
